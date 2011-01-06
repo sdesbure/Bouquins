b56131edf5a2904def46af6eeb3b89dd010625b9
Sylvain Desbureaux <sylvain@desbureaux.fr>
2011-01-06 21:29:35 +0100

    First Commit
    Former-commit-id: 7f91772f9ff8a1d85d57e16efa069c5d74644541

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bouquins::Application.config.secret_token = 'adef6b41ae93fa6886f5322e46d157056f38403584324cc169e53468e9fe150d9983498c14f7ea75b8a2b5ce158eb56bdac7697a5174c370deda855db0e48110'
