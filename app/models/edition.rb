ec9fd0eb515eda842968261e10a58deee8a66821
Sylvain Desbureaux <sylvain@desbureaux.fr>
2011-01-12 21:31:29 +0100

    First Edition Model insertion with factory and relevant spec
    Former-commit-id: 724eae5d8b98a3dd5c72d7562e6ed8ae254fd9fe

---
+++ app/models/edition.rb
@@ -0,0 +1,16 @@
+class Edition
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :isbn, String
+  property :image_url, URI
+  property :year, Date
+
+
+  property :created_at, DateTime
+  # property :created_on, Date
+ 
+  property :updated_at, DateTime
+  # property :updated_on, Date
+
+end
---
+++ spec/support/factories/editions.rb
@@ -0,0 +1,7 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+Factory.define :edition do |f|
+  f.isbn "MyString"
+  f.image_url ""
+  f.year "2011-01-11"
+end
