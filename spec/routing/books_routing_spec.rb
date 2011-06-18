6ce32de2475f5664184a75130968ba2b50a3c865
Sylvain Desbureaux <sylvain@desbureaux.fr>
2011-06-18 23:31:40 +0200

    some styling
    Former-commit-id: b54d99a856909937dd0c7e734d4219bd3cccaa77

--- spec/routing/books_routing_spec.rb
+++ spec/routing/books_routing_spec.rb
@@ -3,9 +3,9 @@ require "spec_helper"
 describe BooksController do
   describe "routing" do
 
- #   it "routes to #index" do
- #     get("/books").should route_to("books#index")
- #   end
+    it "routes to #index" do
+      get("/books").should route_to("books#index")
+    end
 
  #   it "routes to #new" do
  #     get("/books/new").should route_to("books#new")
