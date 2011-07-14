92204e0b00e3fa6b5af365643dc4eb5084c7dc50
Sylvain Desbureaux <sylvain@desbureaux.fr>
2011-07-14 23:05:28 +0200

    Devise et Ajaxful_rating localisé
    Former-commit-id: 9c9a207029ee67968d22dc36c79ddfa746d752b2

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < Devise::RegistrationsController
     else
       build_resource
       clean_up_passwords(resource)
-      flash[:alert] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
+      flash[:alert] = t :recaptcha_error
       render_with_scope :new
     end
   end
