31f569a6657ccd1b1b2673805221876326d58e26
Sylvain Desbureaux <sylvain@desbureaux.fr>
2011-07-17 22:10:03 +0200

    Refactoring cucumber features
    Former-commit-id: 61167f79d53179f8a9f8e1ab794393a9985d1e4f

--- features/step_definitions/my_web_steps.rb
+++
@@ -1,6 +0,0 @@
-Then /^I should see a link to "([^"]*)" with text "([^\"]*)"$/ do |url, text|
-  page.should have_selector("a[href='#{ url }']") do |element|
-    element.should contain(text)
-  end
-end
-
