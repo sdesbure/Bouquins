28a064f011663a937074d87042220b00c061a691
Sylvain Desbureaux <sylvain@desbureaux.fr>
2012-02-19 22:10:32 +0100

    Finishing CRUD display
    Former-commit-id: 3acda2212b30f38205f66b7090920e14fc4ee92b

--- spec/support/factories/readings.rb
+++ spec/support/factories/readings.rb
@@ -1,7 +1,7 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :reading do |f|
-  f.read_book ""
+  f.read_edition ""
   f.reader ""
   f.start_read "2011-06-02 18:44:06"
   f.end_read "2011-06-02 18:44:06"
